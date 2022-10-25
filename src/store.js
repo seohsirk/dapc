b0574ea6f5ac28f20d40daf48ca01fc5cbb10909
seohsirk <seohsirk@gmail.com>
2022-10-25 14:59:49 +0900

    aa

--- src/store.js
+++ src/store.js
@@ -1,5 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+import data from './data/data.js';
+
+let user = createSlice({
+  name: 'user',
+  initialState: 'kim',
+  reducers: {
+    changeName(state) {
+      return 'john' + state
+    },
+  }
+})
+
+export let { changeName } = user.actions
+
+let stock = createSlice({
+  name: 'stock',
+  initialState: [10,20,30]
+})
+
+let item = createSlice({
+  name: 'item',
+  initialState: data
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: {
+    user: user.reducer,
+    stock: stock.reducer,
+    item: item.reducer,
+  }
 }) 
