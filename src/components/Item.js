b97155aec98be15a16014fb1e192eec504db94f0
seohsirk <seohsirk@gmail.com>
2022-11-06 13:19:09 +0900

    ㅁㅁ

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,12 +5,15 @@ function Item(props) {
 
   let navigate = useNavigate();
   let url = ('./detail/' + props.item.article_id).toString();
-
+  let imgList = props.item.img_list.split("|");
+  console.log(imgList[0]);
+  let img = imgList ? imgList[0] : defaultImg;
+  
   return (
     <div onClick={() => {
         navigate(url);
       }} className="item">
-      <img className="item-img" src={defaultImg} />
+      <img className="item-img" src={img} />
       <div className="title">{props.item.title}</div>
       <div className="written-date">
         <span>{props.item.written_date}</span>&nbsp;&nbsp;|&nbsp;&nbsp;<span>{props.item.region}</span></div>
